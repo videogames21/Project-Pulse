ba70e61e6ab4006c0bc418d85cbf30a984274584
Dynangel2006 <angelranget@icloud.com>
2026-04-13 16:48:10 -0500

    initial commit

---
+++ src/test/java/edu/tcu/cs/projectpulse/ProjectPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.projectpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
