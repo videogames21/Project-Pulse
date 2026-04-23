39a00a112fa6077b88e29f0619419ee5b53fc3ae
videogames21 <gtwhittingham@gmail.com>
2026-04-23 17:43:29 -0500

    Update TeamRepository.java
    Potential Fix For TeamRepository

--- backend/src/main/java/edu/tcu/cs/projectpulse/team/TeamRepository.java
+++ backend/src/main/java/edu/tcu/cs/projectpulse/team/TeamRepository.java
@@ -2,15 +2,11 @@ package edu.tcu.cs.projectpulse.team;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
+import java.util.List;
 import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<TeamEntity, Long>, JpaSpecificationExecutor<TeamEntity> {
     boolean existsByName(String name);
     Optional<TeamEntity> findFirstByMembersId(Long userId);
-import java.util.List;
-
-public interface TeamRepository extends JpaRepository<TeamEntity, Long>, JpaSpecificationExecutor<TeamEntity> {
-    boolean existsByName(String name);
     List<TeamEntity> findAllBySectionNameOrderByNameAsc(String sectionName);
 }
