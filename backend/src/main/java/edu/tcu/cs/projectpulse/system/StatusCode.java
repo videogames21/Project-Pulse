2b3e898ea677685e65eebf88077de2b7f9023bb8
videogames21 <gtwhittingham@gmail.com>
2026-04-22 20:22:02 -0500

    Update StatusCode.java
    Removed Duplicate StatusCodes

--- backend/src/main/java/edu/tcu/cs/projectpulse/system/StatusCode.java
+++ backend/src/main/java/edu/tcu/cs/projectpulse/system/StatusCode.java
@@ -5,9 +5,7 @@ public class StatusCode {
     public static final int NOT_FOUND = 404;
     public static final int INTERNAL_SERVER_ERROR = 500;
     public static final int INVALID_ARGUMENT = 400;
-    public static final int NOT_FOUND = 404;
     public static final int CONFLICT = 409;
-    public static final int INTERNAL_SERVER_ERROR = 500;
 
     private StatusCode() {}
 }
